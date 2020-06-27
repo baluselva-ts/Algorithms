c148adb79eb21c05a81dd2fccbc248879f5731fe
baluselva <baluselva.ts@gmail.com>
2020-06-27 18:29:23 +0530

    Updated SLLTemplate

--- Templates/SinglyLinkedListTemplate.cpp
+++ Templates/SinglyLinkedListTemplate.cpp
@@ -6,24 +6,61 @@
 using namespace std;
 
 struct node{
-	int value;
+	int data;
 	struct node *next;
 };
 
 struct node *createNode(int value) {
 	struct node *node = (struct node*)malloc(sizeof(struct node));
-	node -> value = value;
+	node -> data = value;
 	node -> next = NULL;
 	return node;
 }
 
 struct node *createNode(int value, struct node *nextNode) {
 	struct node *node = (struct node*)malloc(sizeof(struct node));
-	node -> value = value;
+	node -> data = value;
 	node -> next = nextNode;
 	return node;
 }
 
+struct node *createList(int listSize) {
+	int value = 0;
+	struct node *head, *temp, *newNode;
+
+	cout << "Enter the list values: " << endl;
+	for (int i = 0; i < listSize; i++) {
+		cin >> value;
+		if (i == 0) {
+			head = createNode(value);
+			temp = head;
+		} else {
+			newNode = createNode(value);
+			temp -> next = newNode;
+			temp = temp -> next;
+		}
+	}
+	return head;
+}
+
+void printList(struct node *head) {
+	struct node *temp = head;
+
+	while (temp != NULL) {
+		cout << temp -> data << " ";
+		temp = temp -> next;
+	}
+	cout << endl;
+}
+
 int main() {
+	int listSize = 0;
+	cout << "Enter the list size: ";
+	cin >> listSize;
+
+	struct node *head = createList(listSize);
+	cout << "List Unchanged: ";
+	printList(head);
+
 	return 0;
 }
