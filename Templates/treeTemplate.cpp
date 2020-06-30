32638d0dae298fc1d10feafe2bb9fb39da139fe5
baluselva <baluselva.ts@gmail.com>
2020-06-30 11:10:04 +0530

    updated treeTemplate

--- Templates/treeTemplate.cpp
+++ Templates/treeTemplate.cpp
@@ -2,6 +2,7 @@
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
 #define ll long long
+#define NODE struct node*
 
 using namespace std;
 
@@ -13,7 +14,7 @@ struct node{
 
 struct node *createNode(int value) {
 	struct node *node = (struct node*)malloc(sizeof(struct node));
-	node -> data = data;
+	node -> data = value;
 	node -> left = NULL;
 	node -> right = NULL;
 	return node;
