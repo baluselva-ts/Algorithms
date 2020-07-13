bbcd1293f61ce650c1dde7ffdb8e5338217c3d36
baluselva <baluselva.ts@gmail.com>
2020-07-13 20:09:39 +0530

    StockBuySell-https://practice.geeksforgeeks.org/problems/stock-buy-and-sell/0

---
+++ Arrays/StockBuySell.cpp
@@ -0,0 +1,41 @@
+//https://practice.geeksforgeeks.org/problems/stock-buy-and-sell/0
+#include <iostream>
+#include <queue>
+#include <map>
+#include <stack>
+#include <unordered_set>
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+#define ll long long
+
+using namespace std;
+
+void getMaxProfitDays(vector<int> inputArray, int inputSize) {
+	int start = 0, i;//, end = 1;
+
+	for (i = 0; i < inputSize - 1; i++) {
+		if (inputArray[i] > inputArray[i + 1]) {
+			if (i - start > 1)
+				cout << start << " -> " << i << endl;
+			start = i + 1;
+		}
+	}
+	if (i - start > 1)
+		cout << start << " -> " << i << endl;
+}
+
+int main() {
+	int inputSize = 0;
+	cout << "Enter array size: ";
+	cin >> inputSize;
+
+	vector<int> inputArray(inputSize);
+	cout << "Enter array elements: ";
+
+	for (int i = 0; i < inputSize; i++)
+		cin >> inputArray[i];
+
+	getMaxProfitDays(inputArray, inputSize);
+
+	return 0;
+}
