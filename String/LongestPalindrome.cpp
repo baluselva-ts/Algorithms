151f45caa69342f689e7c3d9cc37366420704db4
baluselva <baluselva.ts@gmail.com>
2020-07-18 11:56:38 +0530

    LongestPalindrome-https://www.geeksforgeeks.org/longest-palindrome-substring-set-1/

--- String/LongestPalindrome.cpp
+++ String/LongestPalindrome.cpp
@@ -2,6 +2,8 @@
 //without extra space - https://www.geeksforgeeks.org/longest-palindromic-substring-set-2/?ref=rp
 #include <iostream>
 #include <vector>
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
 
 using namespace std;
 
@@ -11,6 +13,7 @@ int getTableValue(vector <vector<int> > table, int i, int j) {
 	return table[i][j];
 }
 
+//dynamic
 void printLongestPalindrome(string inputString) {
 	int strLen = inputString.length();
 	vector <vector<int> > table(strLen, vector<int>(strLen, 0));
@@ -31,12 +34,26 @@ void printLongestPalindrome(string inputString) {
 	cout << inputString.substr(startIndex, maxLengthPalindrome) << endl;
 }
 
+//recursion
+int getPalindrome(string inputString, int start, int end, int length) {
+	if (start > end)
+		return length;
+	if (start == end)
+		return (length + 1);
+	if (inputString[start] == inputString[end]) {
+		length = getPalindrome(inputString, start + 1, end - 1, length + 2);
+		return MAX(length, MAX(getPalindrome(inputString, start, end - 1, 0), getPalindrome(inputString, start + 1, end, 0)));
+	}
+	return MAX(getPalindrome(inputString, start, end - 1, 0), getPalindrome(inputString, start + 1, end, 0));
+}
+
 int main() {
 	string inputString;
+	int length = 0;
 	cout << "Enter input string: ";
 	cin >> inputString;
 
 	printLongestPalindrome(inputString);
-
+	cout << getPalindrome(inputString, 0, inputString.length() - 1, length) << endl;
 	return 0;
 }
